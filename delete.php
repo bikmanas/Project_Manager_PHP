552eeaa5b4b0044c34387701c242ea097d26fe75
Povilas <p.bikmanas@gmail.com>
2021-07-14 17:08:34 +0300

    Fix delete button

--- delete.php
+++ delete.php
@@ -7,7 +7,7 @@ $dbname = "sprintas";
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 var_dump($_POST);
 if (isset($_POST['delete']) and $_POST['path'] == 'employees' and $_POST['delete'] == 'Delete') {
-
+    //Delete option not working TODO;
     $sql = 'DELETE FROM employees_projects WHERE employees_id = ?';
     $stmt = $conn->prepare($sql);
     $stmt->bind_param('employee_id', $_POST['names_id']);
